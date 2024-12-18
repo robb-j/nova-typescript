91ac7e8eada3708db5f3d37b1dc504481ad32c4b
Rob Anderson <robb-j@users.noreply.github.com>
2024-12-18 09:49:53 +0000

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,8 +3,11 @@
 # Ensure the build fails if TypeScript fails
 set -e
 
+# Install development dependencies
+npm install --no-audit
+
 # Install extension dependencies for development
-npm --prefix TypeScript.novaextension i --no-audit
+npm --prefix TypeScript.novaextension install --no-audit
 
 # Build TypeScript into JavaScript after linting
 npx tsc
